89eabfab370cd17ca5e9bcea0d0ef6316c320015
Talha Amjad <myown4500@gmail.com>
2026-04-04 18:09:27 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+
+const express = require("express");
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Sample API route
+app.get("/api/data", (req, res) => {
+  res.json({
+    message: "This is a sample API response",
+  });
+});
+
+// POST route example
+app.post("/api/data", (req, res) => {
+  const body = req.body;
+  res.json({
+    message: "Data received",
+    data: body,
+  });
+});
+
+// Server port
+const PORT = 3000;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
